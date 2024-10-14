b0ac34f4b9eeacf494f68e4421def3fd320eaffe
apoxu <zihangxu123@gmail.com>
2024-10-15 00:16:01 +0800

    more tweaks and ignore lazy-lock.json

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,17 +2,23 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- local map = vim.keymap.set
-vim.keymap.del({ "x", "n" }, ",")
+local map = vim.keymap
+map.del({ "x", "n" }, ",")
 
 -- disable macro recording of neovim
-vim.keymap.set("n", "q", "<Nop>", { noremap = true })
+map.set("n", "q", "<Nop>", { noremap = true })
 
 local wk = require("which-key")
-vim.keymap.set("n", ",", '<cmd>lua require("which-key").show(",", {mode = "n", auto = true})<cr>')
-wk.register({
-  [","] = {
-    w = { "<cmd>w<cr>", "Save current buffer" },
+map.set("n", ",", '<cmd>lua require("which-key").show(",", {mode = "n", auto = true})<cr>')
+wk.add({
+  { ",w", "<cmd>w<cr>", desc = "Save current buffer" },
+  { ",h", "<cmd>noh<cr>", desc = "Disable highlight until next search" },
+  {
+    ",t",
+    function()
+      require("lazyvim.util.terminal").open({})
+    end,
+    desc = "Floting terminal",
   },
-  ["<leader>;"] = { '<cmd>lua require("alpha").start()<cr>', "Dashboard" },
+  { "<leader>;", "<cmd>Dashboard<cr>", desc = "Dashboard" },
 })
